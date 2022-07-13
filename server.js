ac1782290a53d44fd928eb9bd741f515dfeea929
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-07-13 16:27:02 -0700

    local with production run

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://glowz.vercel.app",
+    origin: ["http://localhost:3000", "https://glowz.vercel.app"],
     credentials: true,
   })
 );
