868e674e9d11035adc26aeeda87e1bc664f55891
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-07-14 14:10:29 -0700

    routes

--- models/user.js
+++ models/user.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    index: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
